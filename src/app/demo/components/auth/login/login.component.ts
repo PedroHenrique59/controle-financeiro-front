806ba9735e7d91fd1239a4dbfce9364e606a168d
Pedro Henrique <pedro.henrique59@outlook.com>
2024-04-18 18:46:16 -0300

    Criando comunicação para realizar o login

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import {Component, OnInit} from '@angular/core';
+import {LayoutService} from 'src/app/layout/service/app.layout.service';
+import {HttpClient} from "@angular/common/http";
+import {LoginService} from "../../../service/login.service";
+import {TokenVO} from "../../../model/TokenVO";
+import {UserCredentialsVO} from "../../../model/UserCredentialsVO";
 
 @Component({
     selector: 'app-login',
@@ -7,17 +11,38 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
     styles: [`
         :host ::ng-deep .pi-eye,
         :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
+            transform: scale(1.6);
             margin-right: 1rem;
             color: var(--primary-color) !important;
         }
     `]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
     valCheck: string[] = ['remember'];
 
-    password!: string;
+    userCredentials: UserCredentialsVO;
+    token: TokenVO;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService, private loginService: LoginService) {
+    }
+
+    ngOnInit(): void {
+        this.token = new TokenVO();
+        this.userCredentials = new UserCredentialsVO();
+    }
+
+    signIn() {
+
+        this.loginService.signIn(this.userCredentials).subscribe({
+            next: data => {
+                this.token = <TokenVO>data;
+                localStorage.setItem("token", this.token.accessToken);
+            }, error() {
+                console.error("Login Failed. Try Again");
+            }
+        })
+    }
+
+
 }
---
+++ src/app/demo/service/login.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {ApiConstants} from "../constants/api-constants";
+import {UserCredentialsVO} from "../model/UserCredentialsVO";
+import {Observable} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    signIn(userCredentials: UserCredentialsVO): Observable<any> {
+        return this.http.post(ApiConstants.baseLogin, userCredentials);
+    }
+}
