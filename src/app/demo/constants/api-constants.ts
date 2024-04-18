9c7415b3e11b6f26b082e0eb2df06ab7098593ef
Pedro Henrique <pedro.henrique59@outlook.com>
2024-04-18 18:45:25 -0300

    Criando classe de cosntantes

---
+++ src/app/demo/constants/api-constants.ts
@@ -0,0 +1,11 @@
+export class ApiConstants {
+
+    public static get baseUrl(): string {
+        return 'http://localhost:8081/api/'
+    }
+
+    public static get baseLogin(): string {
+        return this.baseUrl + 'login/signin'
+    }
+
+}
