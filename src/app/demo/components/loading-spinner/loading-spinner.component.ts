693674d657efa1b2d23c4f15a9fba27b78845402
Pedro Henrique <pedro.walter@cnj.jus.br>
2024-10-29 15:42:14 -0300

    Adicionando spinner

---
+++ src/app/demo/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [ProgressSpinnerModule],
+  templateUrl: './loading-spinner.component.html'
+})
+export class LoadingSpinnerComponent {
+
+}
